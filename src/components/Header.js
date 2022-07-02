07cf0d7575834a19c0086f0dbfebe735a1ef935e
serq26 <serhat26basak@gmail.com>
2022-07-02 18:35:05 +0300

    Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,102 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header>
+      <img className="logo" src="/images/logo.svg" alt="Disney Plus" />
+      <ul className="menu">
+        <li>
+          <Link to="/">
+            <div className="menu-item">
+              <svg
+                class="MuiSvgIcon-root"
+                focusable="false"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"></path>
+              </svg>
+              <span>Home</span>
+            </div>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">
+            <div className="menu-item">
+              <svg
+                class="MuiSvgIcon-root"
+                focusable="false"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+              </svg>
+              <span>Search</span>
+            </div>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">
+            <div className="menu-item">
+              <svg
+                class="MuiSvgIcon-root"
+                focusable="false"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"></path>
+              </svg>
+              <span>Watchlist</span>
+            </div>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">
+            <div className="menu-item">
+              <svg
+                class="MuiSvgIcon-root"
+                focusable="false"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+              </svg>
+              <span>Originals</span>
+            </div>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">
+            <div className="menu-item">
+              <svg
+                class="MuiSvgIcon-root"
+                focusable="false"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path d="M18 4l2 4h-3l-2-4h-2l2 4h-3l-2-4H8l2 4H7L5 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V4h-4z"></path>
+              </svg>
+              <span>Movies</span>
+            </div>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">
+            <div className="menu-item">
+              <svg
+                class="MuiSvgIcon-root"
+                focusable="false"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path d="M21 3H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h5v2h8v-2h5c1.1 0 1.99-.9 1.99-2L23 5c0-1.1-.9-2-2-2zm0 14H3V5h18v12z"></path>
+              </svg>
+              <span>Series</span>
+            </div>
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+}
