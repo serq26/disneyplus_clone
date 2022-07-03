91f083f989c9a3a0b877fd8222758c9c7d30fb3d
serq26 <serhat26basak@gmail.com>
2022-07-03 22:33:11 +0300

    Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        Website developed by
+        <a target="_blank" rel="noreferrer" href="https://github.com/serq26" title="Serhat Çolak">
+          {" "}
+          Serhat Çolak
+        </a>{" "}
+        - No commercial purposes intended, made only for showing web development techniques and to be included as a piece of portfolio. All copyrights are deserved by Disney.
+      </p>
+    </footer>
+  );
+}
