1bd2abf924662be8401a8ecd224a51809d2d7971
serq26 <serhat26basak@gmail.com>
2022-07-04 23:33:27 +0300

    Skeleton

---
+++ src/components/skeletons/HomeSkeleton.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function HomeSkeleton() {
+  return (
+    <div class="skeleton">
+      <div class="body">
+        <div class="smaller"></div>
+        <div class="smaller"></div>
+        <div class="smaller"></div>
+        <div class="smaller"></div>
+        <div class="smaller"></div>
+      </div>
+    </div>
+  );
+}
